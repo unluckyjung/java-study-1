e68362aa8c74db2cab0810bf04f5da380c0b141f
yoonsung.jung <ybook2012@gmail.com>
2021-03-16 23:00:17 +0900

    refactor : 주석제거

--- src/main/java/techcourse/fp/mission/StreamStudy.java
+++ src/main/java/techcourse/fp/mission/StreamStudy.java
@@ -5,7 +5,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 import java.util.stream.Collectors;
@@ -16,16 +15,9 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        long count = 0;
-//        for (String w : words) {
-//            if (w.length() > 12) count++;
-//        }
-
-        count = words.stream()
+        return words.stream()
                 .filter(s -> s.length() > 12)
                 .count();
-
-        return count;
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -35,21 +27,12 @@ public class StreamStudy {
     }
 
     public static long sumAll(List<Integer> numbers) {
-//        return numbers.stream()
-//                .mapToInt(Integer -> Integer)
-//                .sum();
-
         return numbers.stream()
                 .reduce(Integer::sum)
                 .orElse(0);
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-//        return numbers.stream()
-//                .filter(number -> number > 3)
-//                .mapToInt(number -> number * 2)
-//                .sum();
-
         return numbers.stream()
                 .filter(number -> number > 3)
                 .map(number -> number * 2)
@@ -62,8 +45,6 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-//        System.out.println(words);
-//        System.out.println(words.size());
         // TODO 이 부분에 구현한다.
 
         words.stream()
